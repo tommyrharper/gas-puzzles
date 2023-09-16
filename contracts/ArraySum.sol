efe7417562581ae85644d768491b2a43453c6cb3
tommyrharper <thomasrobertharper@gmail.com>
2023-09-16 17:23:28 +0100

    feat: ArraySum solution

--- contracts/ArraySum.sol
+++ contracts/ArraySum.sol
@@ -21,3 +21,26 @@ contract ArraySum {
         return sum;
     }
 }
+
+contract OptimizedArraySum {
+    // Do not modify this
+    uint256[] array;
+
+    // Do not modify this
+    function setArray(uint256[] memory _array) external {
+        require(_array.length <= 10, 'too long');
+        array = _array;
+    }
+
+    // optimize this function
+    function getArraySum() external view returns (uint256) {
+        uint256 length = array.length;
+        if (length == 0) return 0;
+        uint256 sum;
+        for (uint256 i = 0; i < length; i++) {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+}
